501b697679d47a94f612657aff2a1fa0723bcafe
Joe Schafer <joeschaferastula@gmail.com>
2018-05-11 17:51:15 -0500

    shipment form functionality/button for link/styling added

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,6 +2,4 @@ class Shipment < ApplicationRecord
   belongs_to :shop
   validates :status, :destination, :origin, :freight_capacity, :service_value, presence: true
 
-  STATUS = ["received", "open", "completed"]
-
 end
