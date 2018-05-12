8f4735938853d2b4d1aee9eb3edefa18e86c9dcd
Joe Schafer <joeschaferastula@gmail.com>
2018-05-12 08:47:41 -0500

    routes for customer fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users, path: 'users', controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
-  devise_for :customers, path: 'customers', controllers: {sessions: 'customers/sessions'}
 
   get 'dashboard/index'
 
   resources :shops do
       resources :shipments
+      devise_for :customers, path: 'customers', controllers: {sessions: 'customers/sessions', registrations: 'customers/registrations'}
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "dashboard#index", as: 'dashboard'
