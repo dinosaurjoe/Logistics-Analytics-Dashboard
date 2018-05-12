30454feb4194b01cbc500e6b5bec654e8a0b0c46
Joe Schafer <joeschaferastula@gmail.com>
2018-05-12 08:42:10 -0500

    activerecord relation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,9 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  belongs_to :shop
+
+
   protected
 
     def confirmation_required?
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,5 @@
 class Shop < ApplicationRecord
   belongs_to :user
   has_many :shipments, dependent: :destroy
+  has_many :customers
 end
