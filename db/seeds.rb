2ea462c241a9230cec569afefbde378344848cc5
Joe Schafer <joeschaferastula@gmail.com>
2018-05-12 13:47:47 -0500

    removed my shops from nav bar and added Customers to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require "yaml"
+# require "open-uri"
+
+Customer.destroy_all
+
+password = Faker::Internet.password(8)
+OCCUPATION = ["Logistics Manager", "Supplier", "Logistics Assistant", "Sales"]
+GENDER = ["men", "women"]
+NUMBER = (0..99)
+
+5.times do
+  c = Customer.new(
+    shop: Shop.first,
+    email: Faker::Internet.email,
+    password: password,
+    password_confirmation: password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    # occupation: "#{OCCUPATION.sample}"
+    )
+  # u.remote_profile_picture_url = "https://randomuser.me/api/portraits/#{GENDER.sample}/#{rand(NUMBER)}.jpg"
+  c.save!
+
+end
