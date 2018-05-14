21dd67d9881d36a4ff236170753f10bbbba22117
Joe Schafer <joeschaferastula@gmail.com>
2018-05-14 09:25:53 -0500

    private methods for freightcapacities controller

--- app/controllers/freight_capacities_controller.rb
+++ app/controllers/freight_capacities_controller.rb
@@ -1,2 +1,19 @@
 class FreightsCapacitiesController < ApplicationController
+  before_action :find_freight_capacity
+  before_action :find_shipment
+
+  def show
+  end
+private
+  def find_shipment
+    @shipment = Shipment.find(params[:shipment_id])
+  end
+
+  def find_freight_capacity
+    @freight_capacity = FreightCapacity.find(params[:id])
+  end
+
+  def freight_capacity_params
+    params.require(:freight_capacity).permit(:shipment_id, :transportation_type, :container_size, :service_value, :volume)
+  end
 end
