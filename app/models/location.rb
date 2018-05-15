ae7962e420564266dad9e9b182e0a9ea3fe5dae5
Joe Schafer <joeschaferastula@gmail.com>
2018-05-15 13:40:15 -0500

    location geocode set up

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,9 @@
 class Location < ApplicationRecord
   belongs_to :locatable, polymorphic: true
+
+  attr_accessible :address, :latitude, :longitude
+
+  geocoded_by :address
+
+  after_validation :geocode, :if => :address_changed?
 end
